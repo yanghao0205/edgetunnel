38299105a0c5ed63568bc72f171714730e14502c
yanghao0205 <52226844+yanghao0205@users.noreply.github.com>
2024-12-17 16:22:18 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -2,8 +2,8 @@
 import { connect } from 'cloudflare:sockets';
 
 let userID = '';
-let proxyIP = '';
-let sub = '';
+let proxyIP = 'ProxyIP.DE.tp2024.fxxk.dedyn.io';
+let sub = 'VLESS.fxxk.dedyn.io';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
 let subProtocol = 'https';
